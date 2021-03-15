4884f626faf8b64716fb76499401d61f53e2bbb4
Aleix Alfonso <aleixalfonsoilerna@gmail.com>
2021-03-16 00:42:03 +0100

    primer commit real

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'IlernaApp',
       home: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              Container(
+                child: CircleAvatar(foregroundImage: NetworkImage('https://lh3.googleusercontent.com/ogw/ADGmqu9P_gWKNfuN9zn90FR2QD7klCkd9p5EOkW_e42B=s32-c-mo',),),
+                height: 100,
+                width: double.infinity,
+                color: Colors.blue,
+              ),
+              crearApartado('Home',Icons.home),
+              crearApartado('Noticias',Icons.mail),
+            ],
+          ),
+
+        ),
         appBar: AppBar(
           title: Text('Material App Bar'),
         ),
         body: Center(
+          
           child: Container(
-            child: Text('Hello World'),
+            child: Text('Main page'),
           ),
         ),
       ),
     );
+  }
+
+Widget  crearApartado(String text, IconData icono) {
+
+  return ListTile(
+    title: Text('$text'),
+    trailing: Icon(icono),
+    onTap: (){},
+  );
+
+
   }
 }
